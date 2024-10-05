7e9d32a2186707ef6e5fd8d7a968200d63122459
Yankho <ysaliji023@student.wethinkcode.co.za>
2024-10-05 09:47:32 +0200

    wrote logic to get human choice

--- js/gameScript.js
+++ js/gameScript.js
@@ -4,4 +4,19 @@ const choices= ["rock", "paper", "scissors"]
 const randomChoice = choices[Math.floor(Math.random() * choices.length)]
     return randomChoice;
 }
-console.log(getComputerChoice())
+//console.log(getComputerChoice())
+
+function getHumanChoice(){
+    let userChoice;
+    while(true){
+        userChoice = prompt("Rock/Paper/Scissors?: ").toLowerCase();
+        if (userChoice === "rock" || userChoice === "paper" || userChoice === "scissor"){
+                break;
+        }else {
+            alert("Invalid choice! Kindly choose Rock/Paper/Scissors")
+        }
+    }
+    return userChoice;
+
+}
+console.log(getHumanChoice())
