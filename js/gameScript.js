97c82f74bd301a4e1bbe5248007be4ff415a021f
Yankho <ysaliji023@student.wethinkcode.co.za>
2024-10-05 09:21:58 +0200

    wrote logic to get computer choice

--- js/gameScript.js
+++ js/gameScript.js
@@ -1,1 +1,7 @@
 console.log("Hello World")
+function getComputerChoice(){
+const choices= ["rock", "paper", "scissors"]
+const randomChoice = choices[Math.floor(Math.random() * choices.length)]
+    return randomChoice;
+}
+console.log(getComputerChoice())
