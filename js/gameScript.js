6eb142246f8ae4f08a469014751e6b66a664ace9
Yankho <ysaliji023@student.wethinkcode.co.za>
2024-10-06 16:26:16 +0200

    added styling and images

--- js/gameScript.js
+++ js/gameScript.js
@@ -67,6 +67,19 @@ document.addEventListener("DOMContentLoaded", () => {
     updateScore();
 });
 function myFunction() {
-      var element = document.body;
-      element.classList.toggle("dark-mode");
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+
+
+    var sunIcon = document.getElementById('sun-icon');
+    var moonIcon = document.getElementById('moon-icon');
+
+
+    if (element.classList.contains("dark-mode")) {
+        sunIcon.style.display = "none";
+        moonIcon.style.display = "inline";
+    } else {
+        sunIcon.style.display = "inline";
+        moonIcon.style.display = "none";
     }
+}
